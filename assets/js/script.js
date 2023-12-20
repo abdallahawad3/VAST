8ebd7835f656bca3e63d34aca1f92cf97c1339b2
abdallahawad3 <djdgdnnxcbj@gmail.com>
2023-12-20 06:13:53 +0200

    ⚡

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
-let menu = document.querySelector(".menu");
+const menu = document.querySelector(".menu");
 let navbar = document.querySelector(".navbar");
-
+let scrollUp = document.querySelector(".scroll-up");
 menu.addEventListener("click", () => {
   navbar.classList.toggle("active");
+});
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    scrollUp.style.right = "50px";
+    scrollUp.addEventListener("click", () => {
+      window.scrollTo(0, 0);
+    });
+  } else {
+    scrollUp.style.right = "-50px";
+  }
 });
